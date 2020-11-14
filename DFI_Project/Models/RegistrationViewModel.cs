5bc30e6e04bd926e36da6bdf79c66096f73cd667
Lii'zza Sulistio <liizzasulistio@gmail.com>
2020-11-14 21:15:44 +0700

    Create RegistrationViewModel.cs

---
+++ DFI_Project/Models/RegistrationViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DFI_Project.Models
+{
+    public class RegistrationViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(50)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password should be same")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
