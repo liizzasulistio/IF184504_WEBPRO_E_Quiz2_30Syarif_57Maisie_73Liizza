391c1bab18c691b1861fcf5e1b6c1ddeb82c087b
Lii'zza Sulistio <liizzasulistio@gmail.com>
2020-11-14 21:18:21 +0700

    Create LoginViewModel.cs

---
+++ DFI_Project/Models/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DFI_Project.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
